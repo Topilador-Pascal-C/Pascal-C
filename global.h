9d820b01edc1d125362f50da7e92f9a1d4ed70c9
Carolina Ferreira <carol.mf14@gmail.com>
2016-10-10 09:57:24 -0300

    Adding new rules for declaration of variables

--- global.h
+++ global.h
@@ -1,10 +1,10 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
- 
-extern int yylex(); 
-extern int yyparse(); 
+
+extern int yylex();
+extern int yyparse();
 extern void yyerror(const char* s);
 /* Name of current input file */
 char * curfilename;
- 
+
 #endif
