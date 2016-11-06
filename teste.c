87702c54b719fabe16728e5ffa4b3390c95a77ef
Carolina Ferreira <carol.mf14@gmail.com>
2016-11-06 15:07:31 -0200

    old things

---
+++ teste.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+string variavel_String;
+char variavel_Char;
+bool _variavelBool;
+signed char variavelShortint;
+short int variavelSmallint;
+int variavelLongint_;
+long long variavel_Int64;
+unsigned char variavelByte;
+unsigned short int VariavelWord;
+unsigned int variavelLongword;
+unsigned long long variavelqword;
+unsigned int variavelCardinal;
+int variavelInteger;
+float variavelSingle;
+double variavelDouble;
+long double variavelExtended;
+
+int main() {
+if (variavelShortint == variavelDouble) {
+}
+variavel_String = teste;
+if (variavelShortint != variavelDouble) {
+}
+variavel_String = teste;
+if (variavelShortint >= variavelDouble) {
+}
+variavel_String = teste;
+if (_variavelBool) {
+}
+variavel_String = "teste";
+
+	return 0;
+} q
